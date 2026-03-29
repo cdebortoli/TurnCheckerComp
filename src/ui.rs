2fc53b96a0507841b0584fe55cf3c5c08f3f2fe4
cdebortoli <debortolichris@gmail.com>
2026-03-29 16:45:45 +0200

    Add some comments

--- src/ui.rs
+++ src/ui.rs
@@ -44,25 +44,30 @@ impl TurnCheckerApp {
 
 impl eframe::App for TurnCheckerApp {
     fn ui(&mut self, ui: &mut egui::Ui, _frame: &mut eframe::Frame) {
+        // Check if ready and so that the server must be running
         self.startup
             .ensure_started(&mut self.runtime, self.pairing.pairing_state());
+        // If server started but server_connection data not processed, configuring pairing system/view
         self.startup.sync_pairing_connection(&mut self.pairing);
 
+        // UI
         ui.heading("Turn Checker Companion");
         ui.separator();
 
         if !self.startup.is_ready() {
-            self.startup.show_status(ui);
+            self.startup.show_status(ui); // Database restore warning
         } else if self.pairing.is_paired() {
-            self.content.show(ui);
+            self.content.show(ui); // Show content
         } else if self.startup.server_started() {
-            self.pairing.show_waiting(ui);
+            self.pairing.show_waiting(ui); // Pairing process
         } else {
-            ui.label("Starting the local sync server...");
+            ui.label("Starting the local sync server..."); // Error
         }
 
+        // Database restore process
         self.startup
             .show_restore_modal(ui, &mut self.runtime, self.pairing.pairing_state());
+        // Update pairing if server was started
         self.startup.sync_pairing_connection(&mut self.pairing);
     }
 }
